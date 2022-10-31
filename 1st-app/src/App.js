fcbea734fd28045a9457ab29558d82e78ea0dedd
João E. Pereira Jr <joaoepj@gmail.com>
2022-10-31 13:58:03 -0300

    Trying some CSS

--- 1st-app/src/App.js
+++ 1st-app/src/App.js
@@ -5,22 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
       <MyComponent/>
-      <MyComponent2 param="blue"/>
-      <MyComponent3 param="red"/>  
+      
+      <MyComponent2 className="App-fs" param="blue"/>
+      <MyComponent3 param="red"/>
+      <MyComponent3 param="green"/>
       </header>
+             
     </div>
   );
 }
@@ -33,7 +25,7 @@ function MyComponent() {
 }
 
 function MyComponent2({param}) {
-  return <h1 style={{color: param}}>I am a component!</h1>
+  return <h2 style={{color: param}}>I am a component!</h2>
   
 }
 
